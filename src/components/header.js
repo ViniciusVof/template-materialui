1f62a87437d86de3d74359da9b96b202f0499ec7
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2024-04-12 13:46:22 -0300

    Adicionando o Drawer

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import AnchorTemporaryDrawer from './drawer';
  
 function Header() {
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setShowDrawer(!showDrawer);
+  };
     return (
         <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
@@ -19,15 +25,17 @@ function Header() {
               aria-label="menu"
               sx={{ mr: 2 }}
             >
-              {/*  
-              <MenuIcon />
-              */}
+            <MenuIcon onClick={() => toggleDrawer(!showDrawer)} />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Gerenciamento de Projetos
             </Typography>
           </Toolbar>
         </AppBar>
+        <AnchorTemporaryDrawer
+          open={showDrawer}
+          toggleDrawer={() => toggleDrawer(!showDrawer)}
+        />
       </Box>
     );
 }
